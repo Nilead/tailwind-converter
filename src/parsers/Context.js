3e497ecdfc8170adaf95757ee3a3d76b8731e4c1
Vu Nguyen <yellow1912@gmail.com>
2020-10-22 11:12:03 +0700

    Add helper method to get all unmatched rules

--- src/parsers/Context.js
+++ src/parsers/Context.js
@@ -1,5 +1,7 @@
 import {CSSUtilities} from "@/helpers/CSSUtilities";
 
+const cloneDeep = require('clone-deep');
+
 export class Context {
   element = null;
 
@@ -36,6 +38,35 @@ export class Context {
 
   hasParsedProp(prop) {
     return -1 !== this.parsedProps.indexOf(prop);
+  }
+
+  getUnMatchedRules() {
+    let unmatched = {};
+
+    for (let k in this.matchedRules) {
+      if (this.matchedRules.hasOwnProperty(k)) {
+        if (null === this.matchedRules[k].properties || 'object' !== typeof this.matchedRules[k].properties) {
+          continue;
+        }
+
+        unmatched[k] = {properties: {}};
+        for (let h in this.matchedRules[k]) {
+          if ('properties' !== h && this.matchedRules[k].hasOwnProperty(h)) {
+            unmatched[k][h] = this.matchedRules[k][h];
+          }
+        }
+
+        for (let j in this.matchedRules[k].properties) {
+          if (this.matchedRules[k].properties.hasOwnProperty(j)) {
+            if (!this.matchesRule(k, j)) {
+              unmatched[k].properties[j] = this.matchedRules[k].properties[j];
+            }
+          }
+        }
+      }
+    }
+
+    return unmatched;
   }
 
   matchesRule(ruleKey, propKey) {
